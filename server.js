b2de594fb0766b445d92e4a39a321ea715259f3b
Vineet Chaudhary <37034655+vineet-c@users.noreply.github.com>
2018-03-08 21:02:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ res.render('vineet.hbs');
 //res.render('vineet.hbs');
 //});
 
-app.listen.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 console.log('server is up on port 3000');
 });
